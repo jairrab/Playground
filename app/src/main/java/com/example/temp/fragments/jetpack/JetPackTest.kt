1811c1ea60e39b5aea05c36c1e8e1ad8784689ad
jay <jaybarria@gmail.com>
2021-04-03 11:22:06 +0800

    JetPack Exercise

--- app/src/main/java/com/example/temp/fragments/jetpack/JetPackTest.kt
+++ app/src/main/java/com/example/temp/fragments/jetpack/JetPackTest.kt
@@ -3,19 +3,32 @@ package com.example.temp.fragments.jetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class JetPackView : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Hello world!")
+            PreviewGreeting()
         }
     }
+}
 
-    @Composable
-    fun Greeting(name: String) {
+@Preview
+@Composable
+fun PreviewGreeting() {
+    Greeting("Android")
+}
+
+@Composable
+fun Greeting(name: String) {
+    Row() {
         Text (text = "Hello $name!")
+        Text (text = "How are you?")
     }
+
 }
